a5ecf409b713810b7a03cdd60231e88a748c92ac
kerlenmelo <kerlen_1@hotmail.com>
2023-10-31 11:11:17 -0300

    :sparkles: main - alguns vetores e testes adicionados

--- src/ProjetoYoutube.java
+++ src/ProjetoYoutube.java
@@ -1,5 +1,29 @@
 public class ProjetoYoutube {
     public static void main(String[] args) {
+        Video v[] = new Video[3];
+        v[0] = new Video ("Aula 1 de POO");
+        v[1] = new Video ("Aula 12 de Java");
+        v[2] = new Video ("Aula 10 de Python");
+
+        Gafanhoto g [] = new Gafanhoto[2];
+        g[0] = new Gafanhoto("Jubileu",22, "M", "juba");
+        g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
         
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]); //Jubileu assiste Python
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0], v[1]); //Jubileu assiste Java
+        vis[1].avaliar(87f);
+        System.out.println(vis[1].toString());
+
+        /*System.out.println("VÍDEOS\n-----------------------");
+        System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("\nGAFANHOTOS\n------------------------");
+        System.out.println(g[0].toString());
+        System.out.println(g[1].toString());*/
     }
 }
