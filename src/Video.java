c0201b6c2cf699c2f3f20c7d0a59ef6b2ff67124
kerlenmelo <kerlen_1@hotmail.com>
2023-10-31 10:29:04 -0300

    :recycle: Alteração da lógica - método setAvaliacao

--- src/Video.java
+++ src/Video.java
@@ -16,30 +16,41 @@ public class Video implements AcoesVideo {
     public String getTitulo() {
         return titulo;
     }
+    
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+    
     public int getAvaliacao() {
         return avaliacao;
     }
+    
     public void setAvaliacao(int avaliacao) {
-        this.avaliacao = avaliacao;
+        int nova;
+        nova = (int) ((this.avaliacao + avaliacao) / this.views);
+        this.avaliacao = nova;
     }
+    
     public int getViews() {
         return views;
     }
+    
     public void setViews(int views) {
         this.views = views;
     }
+    
     public int getCurtidas() {
         return curtidas;
     }
+    
     public void setCurtidas(int curtidas) {
         this.curtidas = curtidas;
     }
+    
     public boolean getReproduzindo() {
         return reproduzindo;
     }
+    
     public void setReproduzindo(boolean reproduzindo) {
         this.reproduzindo = reproduzindo;
     }
